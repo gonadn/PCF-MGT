2d3c3bba254055d0d2501853e8f5bc09bd5e5c71
Adnan Riaz Gondal <gonadn@gmail.com>
2022-06-21 13:02:51 +0200

    first commit

---
+++ pcfmgt/index.ts
@@ -0,0 +1,55 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import * as React from "react";
+import * as ReactDOM from 'react-dom';
+import { createRoot, Root } from 'react-dom/client';
+import {MGT} from './mgt';
+
+import { Providers } from '@microsoft/mgt-element';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+
+export class pcfmgt implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+    private container: HTMLDivElement;
+    private notifyOutputChanged: () => void;
+    private root: Root;
+    constructor()
+    {
+
+    }
+
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
+    {
+        this.notifyOutputChanged = notifyOutputChanged;
+        //this.props.numberOfFaces = context.parameters.numberOfFaces.raw || DEFAULT_NUMBER_OF_FACES;
+        context.mode.trackContainerResize(true);
+        console.log(context);
+        //let test = context.resources.getResource("right-arrow", this.setImage.bind(this, false, ".png"),this.showError.bind(this));
+        //console.log(test);
+
+        this.container = container;
+
+        
+    }
+
+    public updateView(context: ComponentFramework.Context<IInputs>): void
+    {
+        this.root = createRoot(this.container);
+       
+        this.root.render(React.createElement(
+            MGT,
+            {}
+        ));
+    }
+
+
+    public getOutputs(): IOutputs
+    {
+        return {};
+    }
+
+
+    public destroy(): void
+    {
+        // Add code to cleanup control if necessary
+    }
+}
---
+++ pcfmgt/mgt.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { Component, useRef } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import { Person, People, PeoplePicker } from '@microsoft/mgt-react';
+import { Providers } from '@microsoft/mgt-element';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+import { MgtPerson, MgtPeople } from '@microsoft/mgt-components';
+import { PublicClientApplication } from "@azure/msal-browser";
+import { useMsal } from "@azure/msal-react";
+const arr: any[] = ['adnan.gondal@bouvet.no','fredrik.nilsen@bouvet.no','petter.hansen@bouvet.no']
+export const MGT = React.memo((props: any) => {
+
+    const personRef = useRef<MgtPerson>();
+    const peopleRef = useRef<MgtPeople>();
+    const { instance } = useMsal();
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+
+        Providers.globalProvider = new Msal2Provider({
+            clientId: '',
+            authority: "https://login.microsoftonline.com/",
+            redirectUri: "http://localhost:8181",
+            scopes: ["Directory.Read.All","People.Read","Presence.Read.All","User.Read","User.Read.All"]
+        });
+
+        Providers.globalProvider.getAccessToken({scopes: ["User.Read.All"] });
+
+        // requestProfileData();
+
+        return () => console.log("Unmounting..");
+
+    },[]);
+
+    const requestProfileData = () => {
+        const msalConfig: any  = {
+            auth: {
+                tenantId: 'c317fa72-b393-44ea-a87c-ea272e8d963d',
+                clientId: '40b57631-f041-46bc-838e-1b96cba1fe0e',
+                authority: "https://login.microsoftonline.com/c317fa72-b393-44ea-a87c-ea272e8d963d",
+                redirectUri: "http://localhost:8181"
+            },
+            cache: {
+                cacheLocation: "sessionStorage",
+                storeAuthStateInCookie: true
+            }
+          };
+      
+      
+      let msalInst = new PublicClientApplication(msalConfig);
+      msalInst
+          .acquireTokenSilent({
+            scopes: ["User.Read','User.Read.All','Presence.Read.All','People.Read','Directory.Read.All"],
+            authority: `https://login.microsoftonline.com/c317fa72-b393-44ea-a87c-ea272e8d963d`,
+          })
+          .then((response) => {
+            console.log(response);
+          });
+    }
+
+    const handleSelectionChanged = (e:any) => {
+        setPeople(e.target.selectedPeople);
+    };
+
+    return (
+        <>
+            <h1>MGT Test</h1>
+            <PeoplePicker selectionChanged={handleSelectionChanged}></PeoplePicker>
+
+            <Person personQuery="me" />
+            Selected People: <People people={people} />
+
+            <Person ref={personRef} personQuery="adnan.gondal@bouvet.no" />
+            <People
+               ref={peopleRef}
+               peopleQueries={arr}
+                show-max="3">
+            </People>
+        </>
+    );
+
+});
