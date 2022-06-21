261100b06062b3fc5dc9112ba30ae01b9f2bf298
Adnan Riaz Gondal <gonadn@gmail.com>
2022-06-21 13:46:10 +0200

    first commit

--- pcfmgt/mgt.tsx
+++ pcfmgt/mgt.tsx
@@ -15,56 +15,17 @@ export const MGT = React.memo((props: any) => {
     const { instance } = useMsal();
     const [people, setPeople] = useState([]);
 
-    const peop: any[] = [{
-        id: "1",
-        displayName: "Antonio",
-        mail: "adnan.gondal@bouvet.no",
-        userPrincipalName: "adnan.gondal@bouvet.no",
-        imAddress: "adnan.gondal@bouvet.no"
-        
-        }, {
-        id: "2",
-        displayName: "Daniel",
-        mail: "adnan.gondal@bouvet.no",
-        userPrincipalName: "adnan.gondal@bouvet.no",
-        imAddress: "adnan.gondal@bouvet.no"
-        }];
+
 
     useEffect(() => {
-        console.log('Test...');
+
         Providers.globalProvider = new Msal2Provider({
-            clientId: '40b57631-f041-46bc-838e-1b96cba1fe0e',
-            authority: "https://login.microsoftonline.com/c317fa72-b393-44ea-a87c-ea272e8d963d",
+            clientId: '***ClientID***',
+            authority: "https://login.microsoftonline.com/***TenantId***",
             redirectUri: "http://localhost:8181",
             scopes: ["Directory.Read.All","People.Read","Presence.Read.All","User.Read","User.Read.All"]
         });
     },[]);
-
-    const requestProfileData = () => {
-        const msalConfig: any  = {
-            auth: {
-                tenantId: 'c317fa72-b393-44ea-a87c-ea272e8d963d',
-                clientId: '40b57631-f041-46bc-838e-1b96cba1fe0e',
-                authority: "https://login.microsoftonline.com/c317fa72-b393-44ea-a87c-ea272e8d963d",
-                redirectUri: "http://localhost:8181"
-            },
-            cache: {
-                cacheLocation: "sessionStorage",
-                storeAuthStateInCookie: true
-            }
-          };
-      
-      
-      let msalInst = new PublicClientApplication(msalConfig);
-      msalInst
-          .acquireTokenSilent({
-            scopes: ["User.Read','User.Read.All','Presence.Read.All','People.Read','Directory.Read.All"],
-            authority: `https://login.microsoftonline.com/c317fa72-b393-44ea-a87c-ea272e8d963d`,
-          })
-          .then((response) => {
-            console.log(response);
-          });
-    }
 
     const handleSelectionChanged = (e:any) => {
         setPeople(e.target.selectedPeople);
